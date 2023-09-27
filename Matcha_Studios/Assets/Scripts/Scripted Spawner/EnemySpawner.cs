19b32f2498a2ca3d1a92960b35c2fb29b71619d3
Dania Mohd <44768559+DaniaMohd@users.noreply.github.com>
2023-09-27 12:15:00 +0800

    Fake/scripted Enemy spawner

---
+++ Matcha_Studios/Assets/Scripts/Scripted Spawner/EnemySpawner.cs
@@ -0,0 +1,68 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class EnemySpawner : MonoBehaviour
+{
+    [Header("=== Keys to Spawn Enemies ===")]
+    public GameObject AIEnemyNormal;
+    public GameObject AIEnemyOrbital;
+    public GameObject AIEnemyCarrier;
+
+    // Drag your desired input action to this field in the inspector.
+    public InputActionReference spawnKeyJ; 
+    public InputActionReference spawnKeyK;
+    public InputActionReference spawnKeyL;
+
+    [Header("=== Offset spawn positions ===")]
+    public GameObject player; // get the player.
+    public float spawnDistanceNormal = 25f; // Adjust the distance from the player
+    public float spawnDistanceOrbital = 30f; // Adjust the distance from the player
+    public float spawnDistanceCarrier = 40f; // Adjust the distance from the player
+
+    private void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    void OnEnable()
+    {
+        spawnKeyJ.action.Enable();
+        spawnKeyJ.action.performed += ctx => SpawnObject1();
+
+        spawnKeyK.action.Enable();
+        spawnKeyK.action.performed += ctx => SpawnObject2();
+
+        spawnKeyL.action.Enable();
+        spawnKeyL.action.performed += ctx => SpawnObject3();
+    }
+
+    void OnDisable()
+    {
+        spawnKeyJ.action.Disable();
+        spawnKeyJ.action.performed -= ctx => SpawnObject1();
+
+        spawnKeyK.action.Disable();
+        spawnKeyK.action.performed -= ctx => SpawnObject2();
+
+        spawnKeyL.action.Disable();
+        spawnKeyL.action.performed -= ctx => SpawnObject3();
+    }
+
+    void SpawnObject1()
+    {
+        Vector3 spawnPosition = player.transform.position + (transform.forward * spawnDistanceNormal);
+        Instantiate(AIEnemyNormal, spawnPosition, Quaternion.identity);
+    }
+    void SpawnObject2()
+    {
+        Vector3 spawnPosition = player.transform.position + (transform.forward * spawnDistanceOrbital);
+        Instantiate(AIEnemyOrbital, spawnPosition, Quaternion.identity);
+    }
+    void SpawnObject3()
+    {
+        Vector3 spawnPosition = player.transform.position + (transform.forward * spawnDistanceCarrier);
+        Instantiate(AIEnemyCarrier, spawnPosition, Quaternion.identity);
+    }
+}
