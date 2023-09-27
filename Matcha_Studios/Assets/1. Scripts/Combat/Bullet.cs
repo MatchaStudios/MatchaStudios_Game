0e69966e27d3fa384103a111981c85ddca0ff001
thesgsniper <44768559+DaniaMohd@users.noreply.github.com>
2023-09-27 15:23:58 +0800

    simple enemy death fix?

--- Matcha_Studios/Assets/1. Scripts/Combat/Bullet.cs
+++ Matcha_Studios/Assets/1. Scripts/Combat/Bullet.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class Bullet : MonoBehaviour
 {
     [SerializeField]
-    float damage;
+    public float damage;
     [SerializeField]
     float lifetime;
     [SerializeField]
