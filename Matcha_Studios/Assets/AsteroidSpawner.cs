b60339a93bb7575156e150f2b298bc7c2bdf59f1
5465tsdvgjh <kadokawa.kitanae@gmail.com>
2023-10-31 22:07:54 +0900

    Tweaks to asteroid spawnning

--- Matcha_Studios/Assets/AsteroidSpawner.cs
+++ Matcha_Studios/Assets/AsteroidSpawner.cs
@@ -8,29 +8,86 @@ public class AsteroidSpawner : MonoBehaviour
 
     public int amountAsteroidsToSpawn = 10;
 
-    public float minRandomSpawn = -500;
-    public float maxRandomSpawn = 500;
+    public float minRandomSpawn = -300;
+    public float maxRandomSpawn = 300;
+    public Vector3 minScale = new Vector3(0.5f, 0.5f, 0.5f); // Minimum scale
+    public Vector3 maxScale = new Vector3(5.0f, 5.0f, 5.0f);
+    public PatternType spawnPattern;
+    public float radius = 250.0f; // Radius of the arc
+    public float arcAngle = 80.0f; // Angle of the arc in degrees
+    public float positionVariance = 70f; // Maximum variance in position
 
-    int x,y,z;
+    public enum PatternType
+    {
+        SCATTER,
+        ARC
+    }
+    int x, y, z;
 
     private void Start()
     {
-
+        SpawnAsteroid(spawnPattern);
     }
 
-    void SpawnAsteroid()
+    void SpawnAsteroid(PatternType pattern)
     {
-        for (int i = 0; i < amountAsteroidsToSpawn; i++)
+        if (pattern == PatternType.SCATTER)
         {
-            float randomX = Random.Range(minRandomSpawn, maxRandomSpawn);
-            float randomY = Random.Range(minRandomSpawn, maxRandomSpawn);
-            float randomZ = Random.Range(minRandomSpawn, maxRandomSpawn);
+            for (int i = 0; i < amountAsteroidsToSpawn; i++)
+            {
+                float _foo = Random.Range(minScale.x, maxScale.x);
+                Vector3 randomScale = new Vector3(_foo, _foo, _foo);
 
-            Vector3 randomSpawnPoint =
-                new Vector3(transform.position.x + randomX, transform.position.y + randomY, transform.position.z + randomZ);
+                float randomX = Random.Range(minRandomSpawn, maxRandomSpawn);
+                float randomY = Random.Range(minRandomSpawn, maxRandomSpawn);
+                float randomZ = Random.Range(minRandomSpawn, maxRandomSpawn);
 
-            GameObject tempObj = Instantiate(asteroidObjects[0], randomSpawnPoint, Quaternion.identity);
-            tempObj.transform.parent = this.transform;
+                float randomXRotation = Random.Range(0.0f, 360.0f);
+                float randomYRotation = Random.Range(0.0f, 360.0f);
+                float randomZRotation = Random.Range(0.0f, 360.0f);
+
+
+                Vector3 randomSpawnPoint =
+                    new Vector3(transform.position.x + randomX, transform.position.y + randomY, transform.position.z + randomZ);
+
+                GameObject tempObj = Instantiate(asteroidObjects[0], randomSpawnPoint, Quaternion.identity);
+                tempObj.transform.parent = this.transform;
+                tempObj.transform.localScale = randomScale;
+                tempObj.transform.rotation = Quaternion.Euler(randomXRotation, randomYRotation, randomZRotation);
+            }
+        }
+        if (pattern == PatternType.ARC)
+        {
+            for (int i = 0; i < amountAsteroidsToSpawn; i++)
+            {
+                float _foo = Random.Range(minScale.x, maxScale.x);
+                Vector3 randomScale = new Vector3(_foo, _foo, _foo);
+
+
+
+                float randomXRotation = Random.Range(0.0f, 360.0f);
+                float randomYRotation = Random.Range(0.0f, 360.0f);
+                float randomZRotation = Random.Range(0.0f, 360.0f);
+
+                float angle = i * (arcAngle / (amountAsteroidsToSpawn - 1)) - arcAngle / 2.0f; // Calculate the angle within the wider arc
+
+
+                // Calculate the variance in position for each object
+                float varianceX = Random.Range(-positionVariance, positionVariance);
+                float varianceY = Random.Range(-positionVariance, positionVariance);
+                float varianceZ = Random.Range(-positionVariance, positionVariance);
+
+                float x = transform.position.x+ radius * Mathf.Cos(Mathf.Deg2Rad * angle) + varianceX;
+                float y = transform.position.y+varianceY; // Adjust the y-axis position variance if needed
+                float z = transform.position.z+radius * Mathf.Sin(Mathf.Deg2Rad * angle) + varianceZ;
+
+                Vector3 position = new Vector3(x, y, z);
+
+                GameObject tempObj = Instantiate(asteroidObjects[0], position, Quaternion.identity);
+                tempObj.transform.parent = this.transform;
+                tempObj.transform.localScale = randomScale;
+                tempObj.transform.rotation = Quaternion.Euler(randomXRotation, randomYRotation, randomZRotation);
+            }
         }
     }
 
--- Matcha_Studios/Assets/Scripts/AsteroidGenerator.cs
+++ Matcha_Studios/Assets/Scripts/AsteroidGenerator.cs
@@ -9,13 +9,27 @@ public class AsteroidGenerator : MonoBehaviour
     private Vector3 spawnPoint;
     public GameObject asteroid;
     public float startSafeRange;
+    public Vector3 minScale = new Vector3(0.5f, 0.5f, 0.5f); // Minimum scale
+    public Vector3 maxScale = new Vector3(2.0f, 2.0f, 2.0f);
     private List<GameObject> objectsToPlace = new List<GameObject>();
+    public PatternType spawnPattern;
 
+    public enum PatternType
+    {
+        SCATTER,
+        ARC
+    }
     // Start is called before the first frame update
     void Start()
     {
         for (int i = 0; i < amountToSpawn; i++)
         {
+            // Generate a random scale within the specified range
+            Vector3 randomScale = new Vector3(
+                Random.Range(minScale.x, maxScale.x),
+                Random.Range(minScale.y, maxScale.y),
+                Random.Range(minScale.z, maxScale.z)
+            );
             PickSpawnPoint();
 
             //pick new spawn point if too close to player start
@@ -24,8 +38,9 @@ public class AsteroidGenerator : MonoBehaviour
                 PickSpawnPoint();
             }
 
-            objectsToPlace.Add(Instantiate(asteroid, spawnPoint, Quaternion.Euler(Random.Range(0f,360f), Random.Range(0f, 360f), Random.Range(0f, 360f))));
+            objectsToPlace.Add(Instantiate(asteroid, spawnPoint, Quaternion.Euler(Random.Range(0f, 360f), Random.Range(0f, 360f), Random.Range(0f, 360f))));
             objectsToPlace[i].transform.parent = this.transform;
+            objectsToPlace[i].transform.localScale = randomScale;
         }
 
         asteroid.SetActive(false);
@@ -34,17 +49,17 @@ public class AsteroidGenerator : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
     public void PickSpawnPoint()
     {
         spawnPoint = new Vector3(
-            Random.Range(-1f,1f),
+            Random.Range(-1f, 1f),
             Random.Range(-1f, 1f),
             Random.Range(-1f, 1f));
 
-        if(spawnPoint.magnitude > 1)
+        if (spawnPoint.magnitude > 1)
         {
             spawnPoint.Normalize();
         }
